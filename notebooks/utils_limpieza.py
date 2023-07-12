95267f01711b732a7b2d0c0cd5b4e881f3965d34
Cheva <ichevallier@unc.edu.ar>
2023-07-11 22:56:15 -0300

    paso 3 y otras cositas

--- notebooks/utils_limpieza.py
+++ notebooks/utils_limpieza.py
@@ -1,4 +1,4 @@
-## Funciones auxiliares de limpieza
+# Funciones auxiliares de limpieza y preprocesamiento de los datos
 
 def limpiar_basic(df, cols_drop):
     """
@@ -7,7 +7,7 @@ def limpiar_basic(df, cols_drop):
     'df' = dataframe para dropear
     'cols_drop' = lista de nombres de columns para dropear
     """
-    df_limpio = df.drop(columns=cols_drop)
+    df_limpio = df.drop(columns=cols_drop).copy()
     return df_limpio
 
 
@@ -22,14 +22,32 @@ def renombrar_elementos(df, columna:str, fill_otros:str):
     subcat_model_count = df.groupby(by=[columna])['MODELO'].sum()
     subcat_has_model = subcat_model_count[subcat_model_count>0].index.to_list()
     
-    df[columna] = df[columna].apply(lambda x: x if x in subcat_has_model else fill_otros)
-    return df
+    df_new = df.copy()
+    df_new[columna] = df_new[columna].apply(lambda x: x if x in subcat_has_model else fill_otros)
+    return df_new
 
 
-def anonimizar():
+def anonimizar(df, columna:str):
     """
     Función para anonimizar los datos de las columnas que nos parezcan sensibles
-    [dani] Sugiero que esta función tome una df y una lista de columnas,
-    y a esa lista de columnas le aplique un hash.
+
+    df = dataframe
+    columna = columna donde reemplazar elementos
     """
-    return print('función vacía')
+
+    print(f'Actualizando variable {columna}')
+    dic = {}
+    df_map = df.copy()
+    
+    valunico = df_map[columna].unique()
+    porc10 = int(0.1 * len(valunico))
+    prog = 0
+    for v in range(len(valunico)):
+        df_map[columna] = df_map[columna].replace({valunico[v]: v})
+        dic.update({str(valunico[v]): v})
+
+        if v % porc10 == 0:
+            print(f'\t Progreso del {prog}%')
+            prog += 10
+
+    return df_map, dic
